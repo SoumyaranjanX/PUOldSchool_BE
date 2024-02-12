734809c2201de6278d4556d76385b48cfebc98fd
MANTU KUMAR <mantukumarverma2004@gmail.com>
2024-02-12 22:10:50 +0530

    logout controllar

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -96,3 +96,33 @@ export const Login = asyncHandler(async (req, res) => {
 })
 
 
+
+
+export const logout = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        console.log(req.user),
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+
+

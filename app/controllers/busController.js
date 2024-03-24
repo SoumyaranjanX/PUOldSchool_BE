34f52dcf3cdff5836cf47056d28753f290c7b5cb
MANTU KUMAR <mantukumarverma2004@gmail.com>
2024-03-24 10:44:13 +0530

    bus updates

--- app/controllers/busController.js
+++ app/controllers/busController.js
@@ -5,10 +5,10 @@ export const insertDummy = async (req, res) => {
         // Dummy data
         const dummyBusData = [
             {
-                stopName: 'Ponlait 1',
+                stopName: 'UMISARC',
                 timings: {
-                    towardSJ: ['9:10 AM', '9:35 PM', '10:35 PM', '12:40 PM', '1:30 PM'],
-                    towardLibrary: ['11:10AM', '12:05 PM', '1:33 PM', '3:00 PM'],
+                    towardSJ: ['9:44 AM', '10:14 PM', '12:59 PM', '1:29 PM', '1:59 PM,4:59 PM,5:29 PM'],
+                    towardLibrary: ['9:31 AM', '09:50 AM', '12:38 PM', '1:05 PM', '01:38 PM', '4:21 PM', '04:51 PM'],
                 },
             },
             // Add more dummy data as needed
@@ -29,6 +29,11 @@ export const getBusTimings = async (req, res) => {
     try {
         const { stoppage } = req.body;
 
+        console.log(stoppage)
+        if (!stoppage || stoppage === "") {
+            return res.status(400).json({ message: 'Please Select Bus Stoppage' });
+        }
+
         // Find bus timings based on stoppage and direction with case-insensitive comparison
         const result = await Bus.findOne({
             "stopName": { $regex: new RegExp(stoppage, 'i') },
@@ -43,7 +48,7 @@ export const getBusTimings = async (req, res) => {
             return res.status(200).json(response);
         } else {
             console.log('Stoppage not found:', stoppage);
-            return res.status(404).json({ error: 'Stoppage not found' });
+            return res.status(200).json({ message: 'Stoppage not found' });
         }
     } catch (error) {
         console.error('Error fetching bus timings:', error);

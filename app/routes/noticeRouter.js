c2fb426e161c4044176879454bc1fa49d29664c3
MANTU KUMAR <mantukumarverma2004@gmail.com>
2024-03-30 12:42:00 +0530

    Update noticeRouter.js

--- app/routes/noticeRouter.js
+++ app/routes/noticeRouter.js
@@ -2,8 +2,12 @@ import express from "express";
 
 import { createNotice, getNotice } from "../controllers/noticeController.js";
 const noticeRouter = express.Router();
+import multer from "multer";
 
+const upload = multer({
+    dest: 'temp/',
+});
 
-noticeRouter.post("/creationnotice", createNotice);
+noticeRouter.post("/creationnotice", upload.single('image'), createNotice);
 noticeRouter.get("/getnotice", getNotice);
 export default noticeRouter;
